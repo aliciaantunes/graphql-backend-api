7374d80b3fb06c5b865bc0a763d1896d816d9390
Alícia Antunes <antunesalicia07@gmail.com>
2024-05-21 17:51:03 +0100

    feat: add apollo server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("Hello Fernando")
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import gql from 'graphql-tag';
+
+
+// Defina os tipos do GraphQL
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+  `;
+
+// Defina os resolvers
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+    },
+};
+
+// Crie uma instância do Apollo Server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  // Inicie o servidor
+  startStandaloneServer(server, {
+    listen: { port: 4000 },
+  }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
